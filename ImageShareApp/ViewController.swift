0b44f7220a0c31c9500ef9cd3d6c03cbe639765c
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-02 20:18:11 +0100

    Connected.

--- ImageShareApp/ViewController.swift
+++ ImageShareApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appLabel: UILabel!
+    
+    @IBOutlet weak var emailTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func signinButton(_ sender: Any) {
+    }
+    @IBAction func signupButton(_ sender: Any) {
+    }
+    
 
 }
 
