70b073fc33de9415b78c9d8b01543b42fe6c1035
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-02 23:38:40 +0100

    Perfom segue

--- ImageShareApp/SettingsViewController.swift
+++ ImageShareApp/SettingsViewController.swift
@@ -16,6 +16,7 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func logoutTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toViewController", sender: nil)
         
     }
     
